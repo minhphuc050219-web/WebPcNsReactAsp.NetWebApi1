0abeb2a7c2f71dbabc97f310c3cfddd813ce2b17
minhphuc050219-web <minhphuc050219@gmail.com>
2026-03-25 18:47:09 +0700

    rename Cart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import Department from './admin/Department'
 import Account from './admin/Account'
 import ActicleCategory from './admin/ArticleCategory'
 import Article from './admin/Article'
-import Cart from './admin/Cart'
+import Cart from './admin/CartTemp'
 import CartDetail from './admin/CartDetail'
 
 
